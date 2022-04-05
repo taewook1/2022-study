ed6748dc9adcd0ffcdfccc14da03044d6ac7a55a
taewook1 <aktw96@naver.com>
2022-04-05 18:30:45 +0900

    add: 컴포넌트 방식으로 TodoList 생성

---
+++ ToyApp/TodoList/TodoList.js
@@ -0,0 +1,29 @@
+// params.$target - 해당 컴포넌트가 추가가 될 DOM 엘리먼트
+// params.initalState - 해당 컴포넌트의 초기 상태
+function TodoList(params) {
+    const $todoList = document.createElement('div');
+    const $target = params.$target;
+    $target.appendChild($todoList);
+
+    this.state = params.initalState;
+
+    this.render = () => {
+        // this.state = [{ text: '자바스크립트 공부하기'}, { text: '....'}]
+        
+        // map을 돈 이후에는 아래처럼 만들어집니다.
+        /*
+        *   this.state.map(todo => `<li>${todo.text}</li>`)
+        *   [{text: '자바스크립트 공부하기'}, {text: '....'}] ....
+        *   ['<li>자바스크립트 공부하기</li>', <li>....</li>]  
+        *   join('')
+        *   <li>자바스크립트 공부하기</li><li>.....</li>      
+        */
+        $todoList.innerHTML = `
+        <ul>
+            ${this.state.map(todo => `<li>${todo.text}</li>`).join('')}
+        </ul>
+        `
+    }
+
+    this.render();
+}
