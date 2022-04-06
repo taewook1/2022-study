0862eb2114cd5b4aa1ca5ccf5275c207bb9f79fe
taewook1 <aktw96@naver.com>
2022-04-06 15:59:47 +0900

    Feat: 클라이언트 내에서 액션할 수 있는 TodoForm 컴포넌트 방식으로 생성

---
+++ ToyApp/TodoList/src/TodoForm.js
@@ -0,0 +1,30 @@
+function TodoForm({ $target, onSubmit }) {
+  const $form = document.createElement('form')
+
+  $target.appendChild($form)
+
+  let isInit = false
+
+  this.render = () => {
+    $form.innerHTML = `
+      <input type="text" name="todo"/>
+      <button>Add</button>
+    `
+
+    if (!isInit) {
+      $form.addEventListener('submit', e => {
+        e.preventDefault()
+
+        const $todo = $form.querySelector('input[name=todo]')
+        const text = $todo.value
+        
+        if (text.length > 1) {
+          $todo.value = ''
+          onSubmit(text)
+        }
+      })
+      isInit = true
+    }
+  }
+  this.render()
+}
