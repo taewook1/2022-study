c0f03f2476e26296f4ce9736cd63e64734b07680
taewook1 <aktw96@naver.com>
2022-04-06 16:01:40 +0900

    Feat: app.js 생성 후 기존에 main.js에서 하는 작업을 app.js로 묶음

---
+++ ToyApp/TodoList/src/main.js
@@ -0,0 +1,15 @@
+const data = [
+  {
+      text: '자바스크립트 공부하기'
+  },
+  {
+      text: '자바스크립트 복습하기'
+  }
+]
+
+const $app =  document.querySelector('.app')
+
+new App({
+  $target: $app,
+  initialState: data
+})
